baf838b410feafcad9cd4050e20c24b0771b9d4b
Anna Kappes <julie_jia678@hotmail.com>
2022-09-02 19:49:13 -0400

    Finished Exercises: Model Validation

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,6 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Event {
@@ -21,10 +19,32 @@ public class Event {
     @Email(message = "Invalid email. Try again!")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
+    //Add a field to collect information about where the event will take place. This field should not be null or blank.
+    @NotBlank(message = "Location is required!")
+    @NotNull(message = "Location is required!")
+    private String location;
+
+    //Add a field to collect information about whether an attendee must register for the event or not.
+    // For the purposes of validation practice, make this field only able to be marked as true.
+    @AssertTrue(message = "The event must have attendees register!")
+    private boolean shouldRegister = true;
+
+    //Add a field to collect information about the number of attendees for the event.
+    // Valid values for this field should be any number over zero.
+    @NotNull(message = "At least one attendees is required!")
+    @Min(value=1, message = "At least one attendees is required!")
+    private Integer numberOfAttendees;
+
+    @NotNull(message = "Must have # of coursed between 1 and 3")
+    @Min(value = 1, message = "Must have # of coursed between 1 and 3")
+    @Max(value = 3, message = "Must have # of coursed between 1 and 3")
+    private Integer numberOfFoodCourses;
+
+    public Event(String name, String description, String contactEmail, String location, Integer numberOfAttendees) {
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.location = location;
         this.id = nextId;
         nextId++;
     }
@@ -57,6 +77,38 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isShouldRegister() {
+        return shouldRegister;
+    }
+
+    public void setShouldRegister(boolean shouldRegister) {
+        this.shouldRegister = shouldRegister;
+    }
+
+    public Integer getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(Integer numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
+    }
+
+    public Integer getNumberOfFoodCourses() {
+        return numberOfFoodCourses;
+    }
+
+    public void setNumberOfFoodCourses(Integer numberOfFoodCourses) {
+        this.numberOfFoodCourses = numberOfFoodCourses;
     }
 
     @Override
